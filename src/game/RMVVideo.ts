eefa7dd3f404acac66c485509df6f9cdb5bc4393
👑➕🍋 <50390200+eee555@users.noreply.github.com>
2026-04-03 15:14:33 +0800

    Update RMVVideo.ts

--- src/game/RMVVideo.ts
+++ src/game/RMVVideo.ts
@@ -22,53 +22,53 @@
  Works on all known RMV versions but not earlier UMF files.
  *****************************************************************/
 
-import { BaseVideo, VideoEvent } from '@/game/BaseVideo'
+import { BaseVideo, VideoEvent } from "@/game/BaseVideo";
 
 interface Event {
-  time: number
-  x: number
-  y: number
-  event: 'lc' | 'lr' | 'rc' | 'rr' | 'mc' | 'mr' | 'mv' | 'sc' | 'mt'
+  time: number;
+  x: number;
+  y: number;
+  event: "lc" | "lr" | "rc" | "rr" | "mc" | "mr" | "mv" | "sc" | "mt";
 }
 
 export class RMVVideo extends BaseVideo {
-  protected readonly mName = 'RMVVideo'
-  protected mWidth: number
-  protected mHeight: number
-  protected mMines: number
-  protected mMarks: boolean
-  protected mLevel: number
-  protected mBoard: number[]
-  protected mPlayer: Uint8Array
-  protected mEvents: VideoEvent[] = []
+  protected readonly mName = "RMVVideo";
+  protected mWidth: number;
+  protected mHeight: number;
+  protected mMines: number;
+  protected mMarks: boolean;
+  protected mLevel: number;
+  protected mBoard: number[];
+  protected mPlayer: Uint8Array;
+  protected mEvents: VideoEvent[] = [];
 
   // Mode
-  private mode = 0
+  private mode = 0;
   // Level
-  private level = 0
+  private level = 0;
   // Width
-  private w = 0
+  private w = 0;
   // Height
-  private h = 0
+  private h = 0;
   // Mines
-  private m = 0
+  private m = 0;
   // Number of game events
-  private size = 0
+  private size = 0;
   // Stores board and mine locations
-  private board: number[] = []
+  private board: number[] = [];
   // Questionmarks
-  private qm = 0
+  private qm = 0;
   // Style
-  private nf = 0
+  private nf = 0;
   // Player name
   private name: Uint8Array = new Uint8Array();
   // Game events
-  private video: Event[] = []
+  private video: Event[] = [];
   // Cell size used in mouse movement calculations
-  private squareSize = 16
-  
-  constructor (data: ArrayBuffer) {
-    super(data)
+  private squareSize = 16;
+
+  constructor(data: ArrayBuffer) {
+    super(data);
     this.mWidth = 0;
     this.mHeight = 0;
     this.mMines = 0;
@@ -78,38 +78,37 @@ export class RMVVideo extends BaseVideo {
     this.mPlayer = new Uint8Array();
   }
 
-  
-  async init(){
+  async init() {
     // 解析 RMV 录像
-    
+
     await this.readrmv();
-    
+
     // 设置录像基本信息
-    this.mWidth = this.w
-    this.mHeight = this.h
-    this.mMines = this.m
-    this.mMarks = this.qm !== 0
-    this.mLevel = this.level + 1
-    this.mBoard = this.board
-    
+    this.mWidth = this.w;
+    this.mHeight = this.h;
+    this.mMines = this.m;
+    this.mMarks = this.qm !== 0;
+    this.mLevel = this.level + 1;
+    this.mBoard = this.board;
+
     // 设置玩家名称
-    this.mPlayer = this.name
+    this.mPlayer = this.name;
     // 设置录像事件
     // const eventNames: ('mv' | 'lc' | 'lr' | 'rc' | 'rr' | 'mc' | 'mr')[] = ['mv', 'lc', 'lr', 'rc', 'rr', 'mc', 'mr']
-    
+
     for (let i = 0; i < this.video.length; ++i) {
-      const e = this.video[i]
+      const e = this.video[i];
       // Mouse event
-      
+
       if (e.event && e.x < this.squareSize * this.getWidth()) {
         this.mEvents.push({
           time: e.time,
           mouse: e.event,
           column: Math.floor(e.x / this.squareSize),
           row: Math.floor(e.y / this.squareSize),
-          x: e.x / this.squareSize * 16,
-          y: e.y / this.squareSize * 16
-        })
+          x: (e.x / this.squareSize) * 16,
+          y: (e.y / this.squareSize) * 16,
+        });
       }
     }
   }
@@ -117,14 +116,14 @@ export class RMVVideo extends BaseVideo {
   /**
    * Function is used to read video data
    */
-  private async readrmv () {
+  private async readrmv() {
     // Initialise local variables
     let i, j;
 
     const ms = await import("ms-toollib");
 
     let aa = new ms.RmvVideo(this.mData, "");
-    
+
     aa.parse();
     aa.analyse();
     this.w = aa.column;
@@ -135,38 +134,48 @@ export class RMVVideo extends BaseVideo {
     this.level = aa.level - 3;
     aa.current_time = 1e8;
     let game_board: Array<Array<number>> = aa.game_board;
-    this.board = new Array(this.w * this.h).fill(0)
+    this.board = new Array(this.w * this.h).fill(0);
     let total_10 = 0; // 每扫开的格子数量，看是否等于雷数
-    for(i = 0; i < this.h; i++){
-      for(j = 0; j < this.w; j++){
-        if (game_board[i][j] >= 10){
+    for (i = 0; i < this.h; i++) {
+      for (j = 0; j < this.w; j++) {
+        if (game_board[i][j] >= 10) {
           this.board[i * this.w + j] = 1;
           total_10 += 1;
         }
       }
     }
-    this.name = new TextEncoder().encode(aa.player_identifier)
-    if(total_10 != this.m){
-      this.error('不能播放没有扫完的录像。')
+    this.name = new TextEncoder().encode(aa.player_identifier);
+    if (total_10 != this.m) {
+      this.error("不能播放没有扫完的录像。");
     }
-   
+
     for (let e of aa.events) {
-      if(!e.event.is_mouse()){
+      if (!e.event.is_mouse()) {
         continue;
       }
       const e_mouse = e.event.unwrap_mouse();
-      
+
+      if (e_mouse.mouse != "m3v") {
+        console.log(Math.max(e.time + aa.video_start_time, 0) * 1000, e_mouse.x, e_mouse.y, e_mouse.mouse);
+      }
+
       this.video.push({
-        time: Math.round((Math.max(e.time + aa.video_start_time, 0)) * 1000),
+        time: Math.round(Math.max(e.time + aa.video_start_time, 0) * 1000),
         x: e_mouse.x,
         y: e_mouse.y,
-        event: e_mouse.mouse as "lc" | "rc" | "lr" | "rr" | "mc" | "mr" | "mv" | "sc" | "mt"
-      })
+        event: e_mouse.mouse as
+          | "lc"
+          | "rc"
+          | "lr"
+          | "rr"
+          | "mc"
+          | "mr"
+          | "mv"
+          | "sc"
+          | "mt",
+      });
     }
 
     return 1;
-
   }
 }
-
-
